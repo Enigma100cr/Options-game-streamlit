c0806293327f74f7f3848d41f9107ebed8518ec6
Deven <deven.tathavadekar@gmail.com>
2025-02-27 16:27:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ c = conn.cursor()
 # Create a table for storing trade data if it doesn't exist
 c.execute("""
 CREATE TABLE IF NOT EXISTS trades (
-    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id TEXT,
     date TEXT,
     symbol TEXT,
     trade_type TEXT,
